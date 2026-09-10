29b95259773f9c331c9adfb5d141551dab34d45b
Ivan Belkovskiy <appelupdater@gmail.com>
2026-09-11 02:25:29 +0300

    Fix type errors

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -86,6 +86,7 @@ export async function getNavigationData(usbId: number) {
 }
 
 import { parseBuffer } from "music-metadata";
+import { Prisma } from "@prisma/client";
 
 export async function getTrackID3(trackUrl: string) {
     try {
@@ -158,7 +159,7 @@ export async function getUSBData(): Promise<{ success: boolean; data?: USBFlashI
         const res = await prisma.usb_devices.findMany({});
 
         return {
-            success: true, data: res.map(usb => ({
+            success: true, data: res.map((usb: Prisma.usb_devicesGetPayload<{}>) => ({
                 id: usb.id,
                 name: usb.name,
                 style: {
